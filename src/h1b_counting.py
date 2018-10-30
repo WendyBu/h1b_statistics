9b3f26793181b67582814696396855457744a792
yiwenbu <wybyw@hotmail.com>
2018-10-30 00:46:35 -0500

    add get_top10 function

--- src/h1b_counting.py
+++ src/h1b_counting.py
@@ -3,6 +3,11 @@ from collections import Counter
 
 
 def readfile(inputFile):
+    """
+    Read input file
+    :param inputFile: argv[1], input file
+    :return: the occupation list, state list of certified H1B
+    """
     Certified = []
     SOC_Name = []
     State = []
@@ -10,7 +15,7 @@ def readfile(inputFile):
         with open(inputFile, "r") as f:
             reader = csv.DictReader(f, delimiter=';')  # read rows into a dictionary format
             i = reader.fieldnames                                  # read csv header
-            s = [colname for colname in i if 'STATUS' in colname]  # file structure for status: STATUS; CASE_STATUS;
+            s = [colname for colname in i if 'STATUS' in colname]  # field name of status: STATUS; CASE_STATUS
             for row in reader:
                 if row[s[0]] == "CERTIFIED":
                     Certified.append(row)  # store the certified record in Certified list
@@ -29,35 +34,48 @@ def readfile(inputFile):
     return SOC_Name, State
 
 
-def generate_state_report(State_list, stateReport):
-    total_num_state = len(State_list)
-    State_count = Counter(State_list)
-    State_count = sorted(State_count.iteritems(), key=lambda (k, v): (-v, k))
-    top10_states = State_count[0:10]
-    top10_states = [list(state_count) for state_count in top10_states]
-    for item in top10_states:
-        percentage = round(float(item[1])/float(total_num_state)*100, 1)
+def get_top10(inputList):
+    """
+    get top 10 most common element
+    :param inputList: a list
+    :return: top 10 element with case number and percentage
+    """
+    total_num_element = len(inputList)
+    key_count = Counter(inputList)
+    key_count = sorted(key_count.iteritems(), key=lambda (k, v): (-v, k))
+    top10_element = key_count[0:10]
+    top10_element = [list(key_count) for key_count in top10_element]
+    for item in top10_element:
+        percentage = round(float(item[1]) / float(total_num_element) * 100, 1)
         item.append(percentage)
+    return top10_element
+
+
+def generate_state_report(top10_State, stateReport):
+    """
+    Generate top10 states output file
+    :param State_list: state list of certified H1B
+    :param stateReport: output file of top10 states
+    :return: None
+    """
     with open(stateReport, 'w+') as resultFile:
         resultFile.write("TOP_STATES;NUMBER_CERTIFIED_APPLICATIONS;PERCENTAGE" + "\n")
-        for sublist in top10_states:
+        for sublist in top10_State:
             line = "{};{};{}%\n".format(sublist[0], sublist[1], sublist[2])
             resultFile.write(line)
     pass
 
 
-def generate_job_report(SOC_list, occupationReport):
-    total_num_jobs = len(SOC_list)
-    job_count = Counter(SOC_list)
-    job_count = sorted(job_count.iteritems(), key=lambda (k, v): (-v, k))
-    top10_jobs = job_count[0:10]
-    top10_jobs = [list(job_count) for job_count in top10_jobs]
-    for item in top10_jobs:
-        percentage = round(float(item[1]) / float(total_num_jobs) * 100, 1)
-        item.append(percentage)
+def generate_job_report(top10_SOC, occupationReport):
+    """
+    Generate top 10 occupation output file.
+    :param SOC_list: Occupation list(SOC names)
+    :param occupationReport: output file for top10 occupation
+    :return: None
+    """
     with open(occupationReport, 'w+') as resultFile:
         resultFile.write("TOP_OCCUPATIONS;NUMBER_CERTIFIED_APPLICATIONS;PERCENTAGE" + "\n")
-        for sublist in top10_jobs:
+        for sublist in top10_SOC:
             line = "{};{};{}%\n".format(sublist[0], sublist[1], sublist[2])
             resultFile.write(line)
     pass
@@ -69,11 +87,13 @@ def main():
         sys.exit(1)   # abort because of error
     else:
         inputFile = sys.argv[1]
-        occupation_out = sys.argv[2]
-        top_state_out = sys.argv[3]
+        occupation_output = sys.argv[2]
+        top_state_output = sys.argv[3]
         SOC_name, States = readfile(inputFile)
-        generate_job_report(SOC_name, occupation_out)
-        generate_state_report(States, top_state_out)
+        top10_SOC = get_top10(SOC_name)
+        top10_State = get_top10(States)
+        generate_job_report(top10_SOC, occupation_output)
+        generate_state_report(top10_State, top_state_output)
     pass
 
 
@@ -83,5 +103,5 @@ if __name__ == "__main__":
 
 
 #python ./src/h1b_counting.py ./input/h1b_input.csv ./output/top_10_occupations.txt ./output/top_10_states.txt
-#python ./src/h1b_counting.py ./input/sample_2k_2015.csv ./output/top_10_occupations.txt ./output/top_10_states.txt
-#python ./src/h1b_counting.py ./input/sample_2014.csv ./output/top_10_occupations.txt ./output/top_10_states.txt
+#python ./src/h1b_counting.py ./insight_testsuite/tests/test_3_2015data/input/h1b_input.csv ./output/top_10_occupations.txt ./output/top_10_states.txt
+#python ./src/h1b_counting.py ./insight_testsuite/tests/test_2_2014data/input/h1b_input.csv ./output/top_10_occupations.txt ./output/top_10_states.txt
